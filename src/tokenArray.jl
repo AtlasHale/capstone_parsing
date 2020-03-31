c09cb32273d03e4fc5826e66dab1a8ae72db2841
AtlasHale <connor.a.h.h@gmail.com>
2020-03-31 11:17:18 -0700

    returns array of tokens

--- src/tokenArray.jl
+++ src/tokenArray.jl
@@ -3,22 +3,26 @@ using Tokenize
 function tokenArray(vec::Array{Array,1})
     result = []
     for a in vec
-        tokens = Vector{String}
+        tokens = []
         for s in a
         toks = tokenize(s)
             for t in toks
                 val = Tokens.untokenize(t)
-                if val != " "
+                kind = Tokens.kind(t)
+                if val != " " && val != ""
                     println(val)
-                    # append!(tokens, val)
+                    push!(tokens, val)
                 end
             end
-        println(tokens)
         end
+        append!(result, tokens)
     end
     return result
 end 
 
-s = Array[ ["test string 1", "String 2"], ["test string 2 again", "string 4"] ]
+s = Array[ ["test string 1", "String 2"], 
+            ["another test string, this time 3 with ; a semicolon", "string 4"], 
+            ["¿Does it handle unicode well?", "yup"] 
+        ] 
 
 res = tokenArray(s);
