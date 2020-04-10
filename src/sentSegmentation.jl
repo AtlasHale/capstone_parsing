e8f2bb5763b27b67765906e35a07ac3ba33d269d
adamayala <55039415+adamayala@users.noreply.github.com>
2020-04-10 13:11:23 -0700

    Update sentSegmentation.jl

--- src/sentSegmentation.jl
+++ src/sentSegmentation.jl
@@ -1,16 +1,22 @@
+using WordTokenizers
+
 function sentSeg(s)
+    newArray = []
     temp = []
-    tempSmall = []
     for i in s
-        push!(tempSmall, i)
-        if i[end] == '.' && i != "Mr." && i != "Mrs."
-            push!(temp, tempSmall)
-            tempSmall = []
-        end
+        temp = split_sentences(i)
+        push!(newArray, temp)
     end
-    return temp
+    return newArray
 end
+
+# Testing Materials
+#
+# a = ["Here is a sentence this.  Home Mr. ok m? Another one. Am I done yet? Here are som3 23.323 numbers: today!"
+#     , "Resume #2 is here.  He worked someplace awesome?  He also did some stuff!"
+#     , "Resume #3 is coming!  My wife is playing with our dog now. 23% chance she will leave me"
+#     , "Resume #4?  She does not like it when i make jokes like that 100% of the time! I also ect. think this will work."
+#     , "Resume #5 - John Smith. Will this throw it off.  Smith.  Will Smith.  Detective Will Smith."]
     
-sampleCode = ["Here", "is", "a", "sententce", "this.", "Home", "Mr.", "Ok", "M.", "Another", "one."]
-    
-println(sentSeg(sampleCode))
+# a = sentSeg(a)
+# println(a)
